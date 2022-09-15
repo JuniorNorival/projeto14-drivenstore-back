38464e3ded922858078af9cebd52cc0b4d5afb57
layconjohn <layconjohn@gmail.com>
2022-09-15 17:31:50 -0300

    feat: rota post /pedidos

--- src/controllers/carrinhoController.js
+++ src/controllers/carrinhoController.js
@@ -1,3 +1,5 @@
+import dayjs from "dayjs";
+
 import {mongo} from "../database/db.js";
 
 const db = await mongo();
@@ -15,9 +17,22 @@ const carregarCarrinho = async (req, res) => {
 };
 
 const finalizarCarrinho = async (req, res) => {
-    const session = res.locals.session;
+    const user = res.locals.user;
 
-    res.sendStatus(200);
+    try {
+        const listaCarrinho = await db.collection("carrinho").find({userId: user._id}).toArray();
+
+        await db.collection("pedidos").insertOne({
+            pedidos: listaCarrinho,
+            userId: user._id,
+            user: user.nome,
+            registro: dayjs().format("DD/MM") 
+        })
+        res.sendStatus(201);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Erro ao finalizar o Carrinho")
+    }
 }
 
 export { carregarCarrinho, finalizarCarrinho }
