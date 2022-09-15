5bba6269b2d2f327ec72949db5178c2a28ed3c83
layconjohn <layconjohn@gmail.com>
2022-09-15 18:43:21 -0300

    feat: Rota DELETE /carrinho/:id

--- src/controllers/carrinhoController.js
+++ src/controllers/carrinhoController.js
@@ -35,4 +35,33 @@ const finalizarCarrinho = async (req, res) => {
     }
 }
 
-export { carregarCarrinho, finalizarCarrinho }
+const deletarProduto = async (req, res) => {
+    const idProduto = req.params.id;
+
+    try {
+        const produto = await db.collection("carrinho").findOne({id: idProduto});
+
+        if (!produto) {
+            return res.status(400).send("Produto não está no carrinho!");
+        }
+
+        if (produto.quantidade > 0) {
+            await db.colletion("carrinho").updateOne({_id: produto._id}, {$set: {
+                ...produto,
+                quantidade: (produto.quantidade - 1)
+            }})
+
+            return res.status(201).send("Quantidade do produto reduzida");
+        } 
+
+        await db.collection("carrinho").deleteOne({_id: produto._id});
+
+        res.status(201).send("Produto removido");
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Erro ao deletar o produto");
+    }
+    res.sendStatus(201);
+}
+
+export { carregarCarrinho, finalizarCarrinho, deletarProduto }
--- src/routers/carrinhoRouter.js
+++ src/routers/carrinhoRouter.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
 
-import { carregarCarrinho, finalizarCarrinho } from "../controllers/carrinhoController.js";
+import { carregarCarrinho, finalizarCarrinho, deletarProduto } from "../controllers/carrinhoController.js";
 import { validarTokenExistente } from "../middlewares/validarTokenMiddleware.js";
 import { verificarSessaoDoUsuario } from "../middlewares/verificarSessaoUserMiddleware.js";
 
 const router = Router();
 
+//carrinho
 router.get("/carrinho", validarTokenExistente,  verificarSessaoDoUsuario ,carregarCarrinho);
-
-router.post("/pedidos", validarTokenExistente, verificarSessaoDoUsuario ,finalizarCarrinho)
+router.delete("/carrinho/:id", validarTokenExistente,  verificarSessaoDoUsuario, deletarProduto);
+//Finalizar Carrinho
+router.post("/pedidos", validarTokenExistente, verificarSessaoDoUsuario ,finalizarCarrinho);
 
 export default router;
