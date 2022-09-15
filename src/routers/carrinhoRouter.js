4729ecdf550023742959faab35b7d3ba677714ba
layconjohn <layconjohn@gmail.com>
2022-09-15 16:41:23 -0300

    refactor: Middleware para validação de um token existente

--- src/routers/carrinhoRouter.js
+++ src/routers/carrinhoRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
 import { carregarCarrinho } from "../controllers/carrinhoController.js";
+import { validarTokenExistente } from "../middlewares/validarTokenMiddleware.js";
 
 const router = Router();
 
-router.get("/carrinho", carregarCarrinho);
+router.get("/carrinho", validarTokenExistente, carregarCarrinho);
 
 export default router;
