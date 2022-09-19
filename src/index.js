416f921afebafd7ac455912954fb42c8128ce905
Norival <norivaljunior.jf@gmail.com>
2022-09-19 17:56:21 -0300

    ajustes heroku

--- src/index.js
+++ src/index.js
@@ -17,31 +17,8 @@ import pedidoRouter from "./routers/checkoutRouter.js";
 dotenv.config();
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
-
-//Cors Configuration - Start
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  )
-  if (req.method === "OPTIONS") {
-    res.header(
-      "Access-Control-Allow-Methods",
-      "POST, PUT, PATCH, GET, DELETE"
-    )
-    return res.status(200).json({})
-  }
-  next()
-})
-//Cors Configuration - End
-
-
-app.get("/", (req, res) => {
-  res.send("Tudo certo");
-});
 
 //Rota de Auth
 app.use(authRouter);
@@ -49,14 +26,7 @@ app.use(authRouter);
 app.use(produtosRouter);
 //Rota de Carrinho
 app.use(carrinhoRouter);
-app.delete("/pedido", async function (req, res) {
-  try {
-    await db.collection("pedidos").deleteMany();
-    res.send(ok);
-  } catch (erro) {
-    res.send(erro.message);
-  }
-});
+
 app.use(pedidoRouter);
 app.listen(process.env.PORT, () => {
   console.log("=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.");
